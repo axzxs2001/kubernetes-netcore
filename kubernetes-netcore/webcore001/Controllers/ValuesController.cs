df95b8167940f373b1fbaae3974a775e8c2bbc70
桂素伟 <axzxs2001@163.com>
2019-03-08 09:33:13 +0900

    创建webcore001项目和yaml文件

---
+++ kubernetes-netcore/webcore001/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webcore001.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var osName = Environment.MachineName;
+            var version = Environment.OSVersion.VersionString;
+            return new JsonResult(new { OsName = osName, Version = version });
+        }       
+
+        [HttpGet("/health")]
+        public IActionResult Health()
+        {
+            return Ok();
+        }
+    }
+}
