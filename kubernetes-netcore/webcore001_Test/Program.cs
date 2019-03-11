2a263e4e977d41b2f53a2c576268366c7849d6e8
桂素伟 <axzxs2001@163.com>
2019-03-11 16:50:44 +0900

    用重试机制解决滚动升级请求断掉问题。

---
+++ kubernetes-netcore/webcore001_Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using Polly;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace webcore001_Test
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://192.168.252.54:30501");
+            HttpResponseMessage result = await Policy
+              .Handle<Exception>()
+              .WaitAndRetryAsync(new TimeSpan[] {
+                  TimeSpan.FromSeconds(1),
+                  TimeSpan.FromSeconds(3),
+                  TimeSpan.FromSeconds(5)
+              })
+              .ExecuteAsync(() =>
+              {
+                  Console.WriteLine(DateTime.Now);
+                  var response = client.GetAsync("/waittime?second=60000");
+                  return response;
+              });
+            var content = await result.Content.ReadAsStringAsync();
+            Console.WriteLine("{0},{1}", result.StatusCode, content);
+            Console.ReadLine();
+        }
+    }
+}
